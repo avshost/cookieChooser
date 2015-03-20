f296515b92b0aaa79fd706c251ddeb04cc6df1b0
Terry Pearson <terrypearson@gmail.com>
2015-03-19 19:16:39 -0500

    Partially working

---
+++ cookieChooser.php
@@ -0,0 +1,170 @@
+<?php
+
+/*
+  Plugin Name: Cookie Chooser Plugin
+  Plugin URI: https://github.com/theantichris/wordpress-plugin-boilerplate
+  Description: Sets a cookie based on the line the person selects in the dropdown. Uses short tags to create dropdowns.
+  Version: 0.0.1
+  Author: Terry Pearson
+  Author URI: http://www.avshost.com
+  License: GPL V3
+ */
+
+class CookieChooser {
+	private static $instance = null;
+	private $plugin_path;
+	private $plugin_url;
+    	private $text_domain = '';
+
+	/**
+	 * Creates or returns an instance of this class.
+	 */
+	public static function get_instance() {
+		// If an instance hasn't been created and set to $instance create an instance and set it to $instance.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Initializes the plugin by setting localization, hooks, filters, and administrative functions.
+	 */
+	private function __construct() {
+		$this->plugin_path = plugin_dir_path( __FILE__ );
+		$this->plugin_url  = plugin_dir_url( __FILE__ );
+
+		load_plugin_textdomain( $this->text_domain, false, $this->plugin_path . '\lang' );
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_styles' ) );
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_styles' ) );
+
+		register_activation_hook( __FILE__, array( $this, 'activation' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivation' ) );
+
+		$this->run_plugin();
+	}
+
+	public function get_plugin_url() {
+		return $this->plugin_url;
+	}
+
+	public function get_plugin_path() {
+		return $this->plugin_path;
+	}
+
+    /**
+     * Place code that runs at plugin activation here.
+     */
+    public function activation() {
+
+	}
+
+    /**
+     * Place code that runs at plugin deactivation here.
+     */
+    public function deactivation() {
+
+	}
+
+    /**
+     * Enqueue and register JavaScript files here.
+     */
+    public function register_scripts() {
+	
+	 // Register the script like this for a plugin:
+    	wp_register_script( 'jquerycookie', plugins_url( '/js/jquery.cookie.js', __FILE__ ) );
+ 
+    	// For either a plugin or a theme, you can then enqueue the script:
+    	wp_enqueue_script( 'jquerycookie' );
+
+	}
+
+    /**
+     * Enqueue and register CSS files here.
+     */
+    public function register_styles() {
+
+	}
+
+    /**
+     * Place code for your plugin's functionality here.
+     */
+    private function run_plugin() {
+
+	}
+    
+    public function baztag_func( $atts, $content = "" ) {
+		return "content = $content";
+	}
+
+    public function shortcode_func( $atts ) {
+
+	//Looking for a comma separated list like this: "Store 1:lakeville,Store 2 cool:Farmington"
+	//Colans separate the name from the value in the dropdown. Whatever the value is, that is what
+	//the cookie will be.
+
+	
+
+	$atts = shortcode_atts( array(
+		'name' => 'chooserbox',
+		'options' => ''
+	), $atts, 'chooser' );
+
+	$name=$atts['name'];
+	$options=$atts['options'];
+
+	$optionsArray=explode("|",$options);
+
+	$optionSelectString="<option></option>";
+
+	foreach($optionsArray as $opt){
+		$row=explode(":",$opt);
+		$optionSelectString=$optionSelectString."<option name='$row[1]'>$row[0]</option>";		
+	}
+
+
+	$SELECTOR="$";
+	$outputVar = <<<OUTPUT
+	<script>
+	
+	(function ($) {
+
+	$SELECTOR(document).ready(function(){
+		function cookieChooser$name(i){
+		//checks if the cookie has been set
+		if($SELECTOR.cookie('$name') != null) {
+		    // set the option to selected that corresponds to what the cookie is set to
+		    $SELECTOR('#$name option[value="' + $SELECTOR.cookie('$name') + '"]').attr('selected', 'selected');
+		}
+		// when a new option is selected this is triggered
+		$('.select_class').change(function() {
+		    // new cookie is set when the option is changed
+		    $SELECTOR.cookie('$name', $('#$name option:selected').val(), { expires: 90, path: '/'});
+		});
+	
+		}
+	});
+
+	}(jQuery));
+
+	</script>
+	<select name="$name" id="$name" onchange="cookieChooser$name($name)">
+		$optionSelectString
+	<select>
+OUTPUT;
+	return $outputVar;
+
+}
+
+
+}
+
+CookieChooser::get_instance();
+add_shortcode( 'chooser', array( 'CookieChooser', 'shortcode_func' ) );
+
+
